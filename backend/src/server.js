d3e2b2ddef6b699089dc48f958735292c36b410c
Heiwin-NGK <heiwinganesh15@gmail.com>
2026-06-16 19:24:19 +0530

    add jwt auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,16 +7,26 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+const authRoutes = require("./routes/authRoutes");
+console.log("authRoutes:", authRoutes);
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("EventHub Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Server Route Working"
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
